36a0fc0d9f2eef120bb6e79ab13761da0de95fa3
Aurelain <aurelain@gmail.com>
2023-08-25 18:58:07 +0300

    stabilized the report

--- src/system/analyzeData.js
+++ src/system/analyzeData.js
@@ -1,9 +1,11 @@
 import compareTexts from './compareTexts';
 import cls from '../utils/cls';
+import copyToClipboard from 'copy-to-clipboard';
 
 // =====================================================================================================================
 //  D E C L A R A T I O N S
 // =====================================================================================================================
+let data;
 
 // =====================================================================================================================
 //  P U B L I C
@@ -11,11 +13,13 @@ import cls from '../utils/cls';
 /**
  *
  */
-const analyzeData = (data) => {
-    const {list} = data;
+const analyzeData = (dataObject) => {
+    data = dataObject;
+    const {list, wiki} = dataObject;
+    const hasWiki = Boolean(wiki);
     let markup = '';
     for (let i = 0; i < list.length; i++) {
-        markup += buildRow(list[i], i + 1);
+        markup += buildRow(list[i], i + 1, hasWiki);
     }
 
     document.body.insertAdjacentHTML('beforeend', markup);
@@ -29,10 +33,15 @@ const analyzeData = (data) => {
 /**
  *
  */
-const buildRow = (dataItem, nr) => {
+const buildRow = (dataItem, nr, hasWiki) => {
     const comparison = compareTexts(dataItem.text1, dataItem.text2);
     const {text1, text2, differences} = comparison;
     const isChanged = differences !== 0;
+
+    let wikiButton = '';
+    if (hasWiki && dataItem.title) {
+        wikiButton = `<button class="wikiButton" data-nr="${nr}">Wiki</button>`;
+    }
 
     return `
     <div class='${cls('row', !isChanged && 'collapsed')}'>
@@ -40,9 +49,12 @@ const buildRow = (dataItem, nr) => {
             ${nr}. ${dataItem.title || ''}
             <div class='differences'>${differences} differences</div>
         </div>
-        <div class='comparison'>
-            <div class='text'>${addBreaks(text1)}</div>
-            <div class='text'>${addBreaks(text2)}</div>
+        <div class='content'>
+            <div class='comparison'>
+                <div class='text'>${addBreaks(text1)}</div>
+                <div class='text'>${addBreaks(text2)}</div>
+            </div>
+            ${wikiButton}
         </div>
     </div>
     `;
@@ -63,6 +75,11 @@ const addListeners = () => {
     for (const title of titles) {
         title.addEventListener('click', onTitleClick);
     }
+
+    const wikiButtons = document.querySelectorAll('.wikiButton');
+    for (const wikiButton of wikiButtons) {
+        wikiButton.addEventListener('click', onWikiButtonClick);
+    }
 };
 
 /**
@@ -71,6 +88,20 @@ const addListeners = () => {
 const onTitleClick = (event) => {
     const parent = event.currentTarget.parentNode;
     parent.classList.toggle('collapsed');
+};
+
+/**
+ *
+ */
+const onWikiButtonClick = (event) => {
+    const {nr} = event.currentTarget.dataset;
+    const index = Number(nr) - 1;
+    const {title, text2} = data.list[index];
+    copyToClipboard(text2);
+    const src = data.wiki + '/' + title + '?action=edit';
+    window.open(src, '_blank').focus();
+    const row = event.currentTarget.closest('.row');
+    row.classList.add('collapsed');
 };
 
 // =====================================================================================================================
