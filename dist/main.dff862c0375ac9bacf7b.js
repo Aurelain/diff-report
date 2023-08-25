992228ae02f6c5cf7eec8218fdd8809579059ff1
Aurelain <aurelain@gmail.com>
2023-08-25 13:19:03 +0300

    added a viable hello world

---
+++ dist/main.dff862c0375ac9bacf7b.js
@@ -0,0 +1,1 @@
+console.log("Hello!"),(()=>{"use strict";var r={147:r=>{r.exports={}}},o={};!function e(t){var s=o[t];if(void 0!==s)return s.exports;var n=o[t]={exports:{}};return r[t](n,n.exports,e),n.exports}(147)})();
